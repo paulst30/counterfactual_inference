0b56437135035ea78299d46bfe78b134e2524a89
paulst30 <paul.stricker@rub.de>
2025-04-27 19:55:33 +0200

    modified sanity tests

--- tests/testthat/test-main_function.R
+++ tests/testthat/test-main_function.R
@@ -13,7 +13,7 @@ test_that("sanity check",{
   dta$group <- 0
   dta$group[dta$id==1] <- 15
   dta$group[dta$id==2] <- 16
-  dta$y[dta$group <= dta$t] <- dta$y[dta$group <= dta$t] + 10
+  dta$y[dta$group <= dta$t & dta$id %in% c(1,2)] <- dta$y[dta$group <= dta$t & dta$id %in% c(1,2)] + 10
   
   results <- simple_staggered_did(yname = "y",
                                   tname = "t",
@@ -42,7 +42,24 @@ test_that("sanity check",{
   expect_equal(mean(results$grouptime_treatment_effects$crit_val), 5, tolerance = 0.3)
   
   # are the pretreatment pvalues correct? 
-  #expect_true(results$groupwise_treatment_effects$pre_treatment_p_value > 0.05)
+  expect_true(results$groupwise_treatment_effects$pre_treatment_p_value[1] > 0.05)
+  expect_true(results$groupwise_treatment_effects$pre_treatment_p_value[2] > 0.05)
+  
+  # are one-sided pretreatment pvalues correct? 
+  dta$y[dta$id %in% c(1,2)] <- dta$y[dta$id %in% c(1,2)] + dta$t[dta$id %in% c(1,2)]
+  
+  results <- simple_staggered_did(yname = "y",
+                                  tname = "t",
+                                  gname = "group",
+                                  idname = "id",
+                                  xformula = "x",
+                                  varformula = c("z"),
+                                  data = dta)
+
+
+  expect_true(results$groupwise_treatment_effects$pre_treatment_p_value_left[1] > 0.05)
+  expect_true(results$groupwise_treatment_effects$pre_treatment_p_value_right[1] < 0.05)
+  
 })
 
 
