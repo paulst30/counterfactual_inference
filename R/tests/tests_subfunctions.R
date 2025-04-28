5345a26929ef002bf095b4be66ab51bf02969a5e
paulst30 <130632393+paulst30@users.noreply.github.com>
2025-04-28 11:32:18 +0200

    Delete R/tests directory
    unnecessary test folder

--- R/tests/tests_subfunctions.R
+++
@@ -1,9 +0,0 @@
-# test check_inputs
-
-test_data <- data.frame(y = c(1,1,2,2),
-                        t = c(1,2,3,2),
-                        id = c(1,1,1,2),
-                        g = c(0,0,0,2),
-                        u = c(1,1,1,1))
-
-check_inputs(test_data, yname="y", tname="t", idname="id", gname = "g", unitname= "un")
